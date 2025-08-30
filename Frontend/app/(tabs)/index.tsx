ba0dd88af2d0e1303fc7703d884d32af532e6b24
Danton <dantonyap2016@gmail.com>
2025-08-31 03:35:32 +0800

    Add icon

--- Frontend/app/(tabs)/index.tsx
+++ Frontend/app/(tabs)/index.tsx
@@ -19,7 +19,7 @@ import { process_input } from '../../utils/processInput';
 import { buildSegments, computeCensoredText } from '../../utils/textSegmentation';
 
 export default function App() {
-  const [input, setInput] = useState<string>('My name is Tan Wei Ming, NRIC S1234567A, phone 91234567, postal code 123456, i stay at  Clementi Dr #05-77.');
+  const [input, setInput] = useState<string>('My name is Tan Wei Ming, NRIC S1234567A, phone 91234567, I stay at Clementi Dr #05-77, Singapore 522010');
   const [loading, setLoading] = useState(false);
   const [segments, setSegments] = useState<Segment[] | null>(null);
   const [censoredMap, setCensoredMap] = useState<Record<string, boolean>>({});
